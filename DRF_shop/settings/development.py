6c5d08a3aee45c2dc79883f96766260dbee408e6
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-10-03 20:22:55 +0330

    fix: edit database config

--- DRF_shop/settings/development.py
+++ DRF_shop/settings/development.py
@@ -3,13 +3,16 @@ from DRF_shop.settings.base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DRF_shop_db', 
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': os.environ.get('DATABASE-NAME', 'DRF_shop_db'),
+        'USER': os.environ.get('DATABASE-USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE-PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DATABASE-HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASE-PORT', '5432'),
     }
 }
+
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
