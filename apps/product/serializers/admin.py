8d06903fc2358c76f3c27aa992131d6c7b63f990
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-10-12 12:38:44 +0330

    feat: add serializer for product category

---
+++ apps/product/serializers/admin.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+
+from apps.product.models import Category
+
+
+class CreateCategorySerializer(serializers.ModelSerializer):
+    parent = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        parent = validated_data.pop('parent', None)
+        if parent is None:
+            Category.add_root(**validated_data)
+        else:
+            parent_node = get_object_or_404(Category, pk=parent)
+            instance = parent_node.add_child(**validated_data)
+        return instance
+
+    class Meta:
+        model = Category
+        fields = ('id','title', 'description', 'is_active', 'slug', 'parent')
