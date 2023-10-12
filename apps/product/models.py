fadcb725efa8c5382a8d7830f87d2f019b7d3eeb
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-10-12 20:48:15 +0330

    feat: add manager for public category

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from treebeard.mp_tree import MP_Node
 from django.utils.text import slugify
+from managers import CategoryQuerySet
 
 
 class Category(MP_Node):
@@ -8,6 +9,7 @@ class Category(MP_Node):
     description = models.CharField(max_length=255, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     slug = models.SlugField(unique=True)
+    is_public = CategoryQuerySet
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.slug = slugify(self.title)
