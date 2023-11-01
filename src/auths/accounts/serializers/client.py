64693531b469e2570c1af29a81927ecb0e6f16f5
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-01 22:06:39 +0330

    feat: add Verify OTP Code Serializer

--- src/auths/accounts/serializers/client.py
+++ src/auths/accounts/serializers/client.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import password_validation
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from auths.accounts.models import User
 
@@ -7,7 +8,7 @@ from auths.accounts.models import User
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'full_name', 'password')
+        fields = ('email', 'phone', 'full_name', 'password')
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -18,3 +19,16 @@ class CreateUserSerializer(serializers.ModelSerializer):
         except serializers.ValidationError as error:
             self.add_error('password', error)
         return value
+
+
+class VerifyOTPCodeSerializer(serializers.Serializer):
+    code = serializers.CharField(max_length=4)
+
+    def save(self, validated_data):
+        refresh = RefreshToken.for_user(validated_data)
+        return ({
+            'user_id': validated_data.id,
+            'success': True,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
